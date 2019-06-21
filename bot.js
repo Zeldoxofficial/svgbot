af2f06f5e06fddf9a5c2ea6ef28285565e6c5594
Zeldoxofficial <52073378+Zeldoxofficial@users.noreply.github.com>
2019-06-21 13:54:18 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,79 @@
+const Discord = require('discord.js');
+const moment = require("moment");  
+const fs = require("fs");      
+const dateFormat = require('dateformat');
+const client = new Discord.Client(); 
+const Canvas = require("canvas"); 
+const prefix = "+";
+const token = 'BOT_TOKEN';
+const antihack = JSON.parse(fs.readFileSync('./antihack.json' , 'utf8'));
+const antijoin = JSON.parse(fs.readFileSync('./antijoin.json' , 'utf8'));
+let antibots = JSON.parse(fs.readFileSync('./antibots.json' , 'utf8'));
+const log = JSON.parse(fs.readFileSync('./log.json' , 'utf8'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const devs = ['411613098923786241'];
+
+client.on('message', message => {
+    let argresult = message.content.split(` `).slice(1).join(' ');
+    if (message.content.startsWith(prefix + 'setStreaming')) {
+      if (!devs.includes(message.author.id)) return message.channel.send("<@429972030092476437> only this guy can do restart the bot so don't try again 😉.");
+      message.delete();
+      client.user.setGame(argresult, 'https://twitch.tv/DynastyShop');
+
+    } else if(message.content.startsWith(prefix + 'setWatching')) {
+        client.user.setActivity(argresult,{type: 'WATCHING'});
+
+      } else if(message.content.startsWith(prefix + 'setListening')) {
+        client.user.setActivity(argresult,{type: 'LISTENING'});
+
+      } else if(message.content.startsWith(prefix + 'setPlaying')) {
+        client.user.setActivity(argresult,{type: 'PLAYING'});
+
+      } else if(message.content.startsWith(prefix + 'setName')) {
+        client.user.setUsername(argresult);
+
+      } else if(message.content.startsWith(prefix + 'setAvatar')) {
+        client.user.setAvatar(argresult);
+
+
+      } else if(message.content.startsWith(prefix + 'setStatus')) {
+        if(!argresult) return message.channel.send('`online`, `DND(Do not Distrub),` `idle`, `invisible(Offline)` 🎶 أختر أحد الحالات');
+        client.user.setStatus(argresult);
+
+
+    }
+
+  });
+
+    
+
+
+        
+
+client.login(process.env.BOT_TOKEN);
